77ea45d89ecba8d6a4a7ad177006266c11ecd0a7
F.Contreras <flpcnz@gmail.com>
2017-08-28 12:43:39 +1200

    Further CAT2 aligments

---
+++ config/envconfig-cat2mock.js
@@ -0,0 +1,30 @@
+module.exports = {
+  id: 'CAT2MOCK',
+  appName: 'mobileutility',
+  version: '0.0.1',
+  esiWsUrl: 'http://192.168.200.64:8088/esiwebservice?wsdl',
+  basePath: '/api/mobileutility/v1',
+  getCheckerPath: '/validations/{serialnumber}',
+  postCheckerPath: '/validations',
+  getStoresPath: '/stores',
+  heartbeatPath: '/heartbeat',
+  loggerConfig: {
+    name: 'mobileutility',
+    level: 'debug',
+    src: true,
+    streams: [
+      {
+        stream: process.stdout
+      },
+      {
+        type: 'file',
+        path: './logs/mobileutility.log',
+        period: '1d'
+      }
+    ]
+  },
+  port: 8000,
+  timeout: 5000,
+  esiTimeout: 5000,
+  emulation: true
+}
